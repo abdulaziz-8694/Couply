85a8b07ddd05ccd6441093e77c02fab07052f100
abdulaziz-8694 <cs12b1001@iith.ac.in>
2017-05-03 20:25:05 +0530

    Added progressbar for webview

--- app/src/main/java/com/example/a8aa8a/couply/Activities/ShopWebViewActivity.java
+++ app/src/main/java/com/example/a8aa8a/couply/Activities/ShopWebViewActivity.java
@@ -1,9 +1,14 @@
 package com.example.a8aa8a.couply.Activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.example.a8aa8a.couply.R;
 
@@ -12,6 +17,7 @@ public class ShopWebViewActivity extends AppCompatActivity {
     private Intent intent;
     private String URL;
     WebView webView;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +35,21 @@ public class ShopWebViewActivity extends AppCompatActivity {
         webView = (WebView) findViewById(R.id.webview);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new MyWebViewClient());
+        progressBar = (ProgressBar) findViewById(R.id.webview_progress);
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            webView.setVisibility(View.GONE);
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            progressBar.setVisibility(View.GONE);
+            webView.setVisibility(View.VISIBLE);
+        }
     }
 }
